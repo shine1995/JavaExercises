08dc1904d554513894284bf7dd233db2973fd101
shine1995 <shine.state7@gmail.com>
2017-02-16 13:29:15 -0500

    palindromes

--- src/com/shyneinnovations/Main.java
+++ src/com/shyneinnovations/Main.java
@@ -3,6 +3,20 @@ package com.shyneinnovations;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println(isPalindromeTwo("bababa"));
+    }
+
+    public static boolean isPalindromeTwo(String input) {
+        // First, replaces all spaces in input String with "" (no spaces) and .toLowerCase
+        input = input.replaceAll(" ", "").toLowerCase();
+
+        //
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != input.charAt(input.length() - (i + 1))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
